b53e3a0cc8221c10a1a70de40fa4b87ee8ee8502
Danfred Ciervo <danfredciervo06@gmail.com>
2026-06-11 13:55:44 +0800

    implement Clerk authentication and environment configuration; add CORS support and webhook handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,26 @@
-import express from "express"
+import  "dotenv/config";
+import express from "express";
+import cors from "cors";
 
-const app = express()
 
-app.listen(3001, () => console.log("Listening on port 3001"))
+import { getEnv } from "./middleware/lib/env";
+import { clerkMiddleware } from "@clerk/express";
+import { clerkWebhookHandler } from "./webhooks/clerck";
+
+const env = getEnv();
+const app = express();
+
+const rawJson = express.json({type: "application/json", limit: "lmb"})
+
+app.post("/webhooks/clerk", rawJson,(req, res) => {
+   void clerkWebhookHandler(req, res);
+
+});
+
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
+
+
+
+app.listen(env.PORT, () => console.log("Listening on port:", env.PORT));
